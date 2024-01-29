1d479c0e1c5046704b9bdbc09212933e8f1334ae
rkeerthient <rkeerthi@yext.com>
2024-01-29 13:56:38 +0530

    added contact us form

--- src/components/ContactUs.tsx
+++ src/components/ContactUs.tsx
@@ -2,6 +2,7 @@ import { Fragment, useState } from "react";
 import { Dialog, Transition } from "@headlessui/react";
 import { CheckIcon } from "@heroicons/react/24/outline";
 import * as React from "react";
+import { IoClose } from "react-icons/io5";
 type ContactProps = {
   showPopUp: (value: boolean) => void;
 };
@@ -35,61 +36,72 @@ const ContactUs = (props: ContactProps) => {
               leaveFrom="opacity-100 translate-y-0 sm:scale-100"
               leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
             >
-              <Dialog.Panel className="relative transform overflow-hidden rounded-lg bg-white px-4 pb-4 pt-5 text-left shadow-xl transition-all  w-[700px]  sm:p-6 text-sm">
-                <div className="mx-auto flex h-12 w-12 items-center justify-center rounded-full  ">
-                  <img
-                    src="https://www.rbcroyalbank.com/dvl/v1.0/assets/images/logos/rbc-logo-shield.svg"
-                    alt=""
+              <Dialog.Panel className="  transform  rounded-lg bg-white px-4 pb-4 pt-5 text-left shadow-xl transition-all  w-[700px]  sm:p-6 text-sm">
+                <div className="flex flex-row justify-between w-full">
+                  <div className="mx-auto flex h-44 w-full items-center justify-center rounded-full flex-col gap-4 ">
+                    <img
+                      className="w-16"
+                      src="https://www.rbcroyalbank.com/dvl/v1.0/assets/images/logos/rbc-logo-shield.svg"
+                      alt=""
+                    />
+                    <div className="font-light text-4xl">Contact RBC Team</div>
+                    <div className="text-slate-400 text-lg font-light">
+                      * Required Fields
+                    </div>
+                  </div>
+                  <IoClose
+                    className="hover:cursor-pointer"
+                    onClick={() => {
+                      setOpen(false);
+                      showPopUp(false);
+                    }}
                   />
                 </div>
-                <div className=" mt-5 flex flex-col gap-4 w-full">
-                  <div className="flex justify-between w-full">
-                    <div className="flex flex-col gap-2 w-full">
-                      <div>Your Name*</div>
-                      <div className="flex justify-between w-full gap-6">
-                        <input
-                          type="text"
-                          placeholder="First Name"
-                          className="border rounded-full  p-2 h-8 w-1/2"
-                        />
+                <div className=" mt-5 flex flex-col gap-2 space-y-6 w-full">
+                  <div className="flex flex-col gap-2">
+                    <div className="text-base font-medium">Your Name*</div>
+                    <div className="flex justify-between w-full gap-6">
+                      <input
+                        type="text"
+                        placeholder="First Name"
+                        className="border text-lg border-black rounded-full p-4 h-14 w-1/2"
+                      />
 
-                        <input
-                          type="text"
-                          placeholder="Last Name"
-                          className="border rounded-full  p-2 h-8 w-1/2"
-                        />
-                      </div>
+                      <input
+                        type="text"
+                        placeholder="Last Name"
+                        className="border text-lg border-black rounded-full p-4 h-14 w-1/2"
+                      />
+                    </div>
+                  </div>
+                  <div className="flex flex-col gap-2">
+                    <div className="text-base font-medium">
+                      How can we contact you?*
+                    </div>
+                    <div className="flex w-full">
+                      <input
+                        type="email"
+                        placeholder="Email"
+                        className="border text-lg border-black rounded-full  p-4 h-14 w-full"
+                      />
                     </div>
                   </div>
                   <div className="flex justify-between w-full">
-                    <div className="flex flex-col gap-2 w-full">
-                      <div>How can we contact you?*</div>
-                      <div className="flex w-full">
-                        <input
-                          type="email"
-                          placeholder="Email"
-                          className="border rounded-full  p-2 h-8 w-full"
-                        />
-                      </div>
-                      <div className="flex justify-between w-full">
-                        <div className="flex justify-between w-full gap-6">
-                          <input
-                            placeholder="Work Phone (optional)"
-                            type="phone"
-                            className="border rounded-full  p-2 h-8 w-1/2"
-                          />
+                    <div className="flex justify-between w-full gap-6">
+                      <input
+                        placeholder="Work Phone (optional)"
+                        type="phone"
+                        className="border text-lg border-black rounded-full  p-4 h-14 w-1/2"
+                      />
 
-                          <input
-                            type="phone"
-                            placeholder="Work Phone (optional)"
-                            className="border rounded-full  p-2 h-8 w-1/2"
-                          />
-                        </div>
-                      </div>
+                      <input
+                        type="phone"
+                        placeholder="Work Phone (optional)"
+                        className="border text-lg border-black rounded-full  p-4 h-14 w-1/2"
+                      />
                     </div>
                   </div>
                 </div>
-
                 <div className="mt-5 sm:mt-6">
                   <button
                     type="button"
